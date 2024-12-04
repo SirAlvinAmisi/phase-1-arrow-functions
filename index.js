694477c5cde0b8bbe0ae4d38ae508b1f2c50e021
Alvin Amisi <alvomakaya@gmail.com>
2024-12-04 23:19:29 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  console.log(divide());
+  
+  // 3. Arrow function called square
+  const square = x => x * x;
+  console.log(square(5)); 
+  
+  // 5. Arrow function called add
+  const add = (a, b) => a + b;
+  console.log(add(3, 7)); 
